5fd51227c37043c76cc34d7e9a4889fa38b22a8f
ahmmedrejowan <ahmmedrejowan@gmail.com>
2021-12-18 03:02:05 +0600

    testing

--- app/src/main/java/com/rejowan/cutevisaulizer/Home.java
+++ app/src/main/java/com/rejowan/cutevisaulizer/Home.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.rejowan.cutevisualizer.EqualizerView;
 
+
 public class Home extends AppCompatActivity {
 
     EqualizerView equalizerView;
