87a0e2e87b59e5b912c6055f112430c47edb4578
ahmmedrejowan <ahmmedrejowan@gmail.com>
2021-12-18 04:12:49 +0600

    changed ui for demo

--- app/src/main/java/com/rejowan/cutevisaulizerlib/Home.java
+++ app/src/main/java/com/rejowan/cutevisaulizerlib/Home.java
@@ -10,7 +10,7 @@ import com.rejowan.cutevisualizer.CuteVisualizer;
 
 public class Home extends AppCompatActivity {
 
-    CuteVisualizer cuteVisualizer;
+    CuteVisualizer cuteVisualizer, cuteVisualizer2, cuteVisualizer3, cuteVisualizer4;
     TextView playButton;
     boolean isPlaying = false;
 
@@ -21,20 +21,32 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         cuteVisualizer = findViewById(R.id.equalizerView);
+        cuteVisualizer2 = findViewById(R.id.equalizerView2);
+        cuteVisualizer3 = findViewById(R.id.equalizerView3);
+        cuteVisualizer4 = findViewById(R.id.equalizerView4);
+
         playButton = findViewById(R.id.playButton);
 
         cuteVisualizer.stopBars();
+        cuteVisualizer2.stopBars();
+        cuteVisualizer3.stopBars();
+        cuteVisualizer4.stopBars();
 
         playButton.setOnClickListener(v -> {
             if (isPlaying) {
                 isPlaying = false;
                 playButton.setText("Play");
                 cuteVisualizer.stopBars();
-
+                cuteVisualizer2.stopBars();
+                cuteVisualizer3.stopBars();
+                cuteVisualizer4.stopBars();
             } else {
                 isPlaying = true;
                 playButton.setText("Pause");
                 cuteVisualizer.animateBars();
+                cuteVisualizer2.animateBars();
+                cuteVisualizer3.animateBars();
+                cuteVisualizer4.animateBars();
             }
 
         });
